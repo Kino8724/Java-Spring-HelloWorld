eb1b5c0df4c45a44f0a3e35cfa40def8e7b94faa
Wyatt Ptak <107960001+Kino8724@users.noreply.github.com>
2023-12-16 17:00:42 -0700

    Added DaikichiPractice Files (Route Practice)

---
+++ daikichiPractice/src/test/java/com/codingdojo/daikichipractice/DaikichiPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.daikichipractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaikichiPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
